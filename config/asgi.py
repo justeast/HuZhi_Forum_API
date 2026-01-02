1a691fefbecd67ca6786c0403e0ff7a14179c8bc
qinyingdong <qinyingdong@qq.com>
2026-01-02 20:54:48 +0800

    修改channels导入启动顺序

--- config/asgi.py
+++ config/asgi.py
@@ -8,22 +8,21 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+from django.core.asgi import get_asgi_application
 from pathlib import Path
 from dotenv import load_dotenv
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-import chat.routing
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 load_dotenv(BASE_DIR / '.env')  # 明确到根目录找 .env
 
-from django.core.asgi import get_asgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-
 django_asgi_app = get_asgi_application()
+
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
 
 application = ProtocolTypeRouter({
     # (HTTP 请求) -> Django 视图
